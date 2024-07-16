de4715258dbc0253111a1b18fcd9ceaec1c8ba62
tal.yoffe <tal.yoffe@saucelabs.com>
2024-07-16 07:31:43 -0400

    using tf public reg mod

--- main.tf
+++ main.tf
@@ -3,36 +3,16 @@ provider "aws" {
 }
 
 module "vpc" {
-  source = "git::https://github.com/Coalfire-CF/terraform-aws-vpc-nfw.git"
+  source = "git@github.com:terraform-aws-modules/terraform-aws-vpc.git"
 
-  vpc_name    = "my-vpc"
-  vpc_cidr    = "10.1.0.0/16"
-  azs         = ["us-east-1a", "us-east-1b"]
-
-  public_subnets = [
-    {
-      name = "subnet-1"
-      cidr = "10.1.0.0/24"
-    },
-    {
-      name = "subnet-2"
-      cidr = "10.1.1.0/24"
-    }
-  ]
-
-  private_subnets = [
-    {
-      name = "subnet-3"
-      cidr = "10.1.2.0/24"
-    },
-    {
-      name = "subnet-4"
-      cidr = "10.1.3.0/24"
-    }
-  ]
-
-  enable_nat_gateway = true
-  single_nat_gateway = true
+  name                 = "my-vpc"
+  cidr                 = "10.1.0.0/16"
+  azs                  = ["us-east-1a", "us-east-1b"]
+  private_subnets      = ["10.1.2.0/24", "10.1.3.0/24"]
+  public_subnets       = ["10.1.0.0/24", "10.1.1.0/24"]
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  enable_nat_gateway   = true
 
   tags = {
     Name = "my-vpc"
@@ -47,10 +27,11 @@ output "vpc_id" {
 
 output "public_subnets" {
   description = "The public subnets"
-  value       = module.vpc.public_subnet_ids
+  value       = module.vpc.public_subnets
 }
 
 output "private_subnets" {
   description = "The private subnets"
-  value       = module.vpc.private_subnet_ids
+  value       = module.vpc.private_subnets
 }
+
