adb649b7cbc9871fc6ef553cf5801c952bf50597
tal.yoffe <tal.yoffe@saucelabs.com>
2024-07-16 13:05:45 -0400

    remove module

--- alb.tf
+++ alb.tf
@@ -57,54 +57,48 @@ resource "aws_security_group" "alb_sg" {
   }
 }
 
-module "alb" {
-  source = "git::https://github.com/terraform-aws-modules/terraform-aws-alb.git"
-
+resource "aws_lb" "alb" {
   name               = "application-load-balancer"
+  internal           = false
   load_balancer_type = "application"
-  vpc_id             = module.vpc.vpc_id
+  security_groups    = [aws_security_group.alb_sg.id]
   subnets            = module.vpc.public_subnets
-
-  security_groups = [aws_security_group.alb_sg.id]
-
-  listeners = [
-    {
-      port     = 80
-      protocol = "HTTP"
-      default_action = {
-        type             = "forward"
-        target_group_arn = ""
-      }
-    }
-  ]
-
-  target_groups = [
-    {
-      name_prefix      = "tytg"
-      backend_protocol = "HTTPS"
-      port             = 443
-      target_type      = "instance"
-      vpc_id           = module.vpc.vpc_id
-
-      health_check = {
-        enabled             = true
-        path                = "/"
-        protocol            = "HTTPS"
-        matcher             = "200"
-        interval            = 30
-        timeout             = 5
-        healthy_threshold   = 2
-        unhealthy_threshold = 2
-      }
-    }
-  ]
 
   tags = {
     Name = "application-load-balancer"
   }
 }
 
+resource "aws_lb_target_group" "tg" {
+  name_prefix = "tytg"
+  protocol    = "HTTPS"
+  port        = 443
+  target_type = "instance"
+  vpc_id      = module.vpc.vpc_id
+  health_check {
+    enabled             = true
+    path                = "/"
+    protocol            = "HTTPS"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
+
 resource "aws_autoscaling_attachment" "asg_attachment" {
   autoscaling_group_name = aws_autoscaling_group.example_asg.name
-  lb_target_group_arn    = module.alb.target_groups[0].arn
+  lb_target_group_arn    = aws_lb_target_group.tg.arn
 }
